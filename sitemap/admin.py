d67fbead5a09333af1a7f2ea4e923dd6d98091dc
godqhrenf <godqhrenf@naver.com>
2021-07-31 18:10:56 +0900

    Map visualization and GUI task completed
    For now, map information is restricted to geumjeonggu

---
+++ sitemap/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Node
+from .models import Edge
+
+admin.site.register(Node)
+admin.site.register(Edge)
---
+++ sitemap/views.py
@@ -0,0 +1,126 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import folium
+from folium.features import DivIcon
+from branca.element import Element
+
+import json
+from collections import OrderedDict
+
+import pandas as pd
+import sqlite3
+
+cop_num = 3
+
+cops_cur_node = [1400002200, 1400002600, 1400003300] # ?, 금정구청, ?
+rob_cur_node = 1400002900 # 금정경찰서교차로
+
+turn = 1
+
+is_rob_turn = True
+
+node_df = pd.DataFrame()
+
+# map 구성
+def initMap():
+    global node_df
+
+    # read node data from database
+    engine = sqlite3.connect('./db.sqlite3')
+    node_df = pd.read_sql('SELECT * FROM node_information', engine, index_col='nodeId')
+    node_df['linkedNode'] = node_df['linkedNode'].apply(lambda x : json.loads(x)) # json to list
+
+    # Create map,   지도 중심 금정구청으로 잡음
+    m = folium.Map(location=[35.243603319969786, 129.09212543391874], zoom_start=15)
+
+    # draw graph on map
+    for row_index, row in node_df.iterrows():
+        enode_list = row['linkedNode']
+        for enode in enode_list:
+            fnode_point = [row['latitude'], row['longitude']]
+            enode_point = [node_df.loc[enode, 'latitude'], node_df.loc[enode, 'longitude']]
+            folium.PolyLine([fnode_point, enode_point]).add_to(m)
+    
+    # json file에 저장할 dictionary 만들기
+    linked_node_data = OrderedDict()
+
+    # Show nodes that can be moved by a thief.
+    if is_rob_turn:
+        for i in range(0, len(node_df.loc[rob_cur_node, 'linkedNode'])):
+            linked_node_id = node_df.loc[rob_cur_node, 'linkedNode'][i]
+            folium.Marker([node_df.loc[linked_node_id, 'latitude'], node_df.loc[linked_node_id, 'longitude']],
+                        icon=DivIcon(
+                            icon_size=(150,36),
+                            icon_anchor=(7,20),
+                            html='<div style="background-color: white; width:25px; height:25px; border-radius:50%; text-align:center; line-height:25px; font-size:15px;">'
+                                    +(i).__str__()+'</div>',
+                        )).add_to(m)
+            linked_node_data[i] = linked_node_id
+
+    # json 파일로 저장
+    with open('linkedNode.json', 'w', encoding="utf-8") as make_file:
+        json.dump(linked_node_data, make_file, ensure_ascii=False, indent='\t')
+    
+    # Show Cop and Robber location
+    folium.Marker([node_df.loc[rob_cur_node, 'latitude'], node_df.loc[rob_cur_node, 'longitude']], icon=folium.Icon(icon='car', prefix='fa', color = 'red')).add_to(m)
+    for i in range(0, cop_num):
+        folium.Marker([node_df.loc[cops_cur_node[i], 'latitude'], node_df.loc[cops_cur_node[i], 'longitude']], icon=folium.Icon(icon='star', color = 'blue')).add_to(m)
+
+    # Get html representation of map
+    m = m._repr_html_()
+    return m
+
+
+def map(request):
+
+    global is_rob_turn, cops_cur_node
+    
+    is_finish = False
+
+    for i in range(0, cop_num):
+        if cops_cur_node[i] == rob_cur_node:
+            is_finish = True
+
+    context = {
+        'm': initMap(),
+        'turn' : turn,
+        'is_rob_turn' : is_rob_turn,
+        'linked_node_num' : len(node_df.loc[rob_cur_node, 'linkedNode']),
+        'is_finish' : is_finish,
+    }
+
+    return render(request, 'sitemap/map.html', context)
+
+# Change Cop/Robber location
+def moveNextNode(request):
+    global is_rob_turn, cops_cur_node
+
+    if is_rob_turn:
+        global turn 
+        turn = turn + 1
+
+        global rob_cur_node
+
+        with open('linkedNode.json') as json_file:
+            json_data = json.load(json_file)
+            rob_cur_node = json_data[request.POST['next_node'].__str__()]
+        
+        is_rob_turn = False
+
+    else:
+        for i in range(0, cop_num):
+            cops_cur_node[i] = node_df.loc[cops_cur_node[i], 'linkedNode'][0]
+        is_rob_turn = True
+
+    return render(request, 'sitemap/map.html')
+
+# Init map inform
+def initMapInform(request):
+
+    global turn, cops_cur_node, rob_cur_node, is_rob_turn
+    turn = 1
+    cops_cur_node = [1400002200, 1400002600, 1400003300] # 금정구청
+    rob_cur_node = 1400002900 # 금정경찰서교차로
+    is_rob_turn = True
+
+    return render(request, 'sitemap/map.html')
